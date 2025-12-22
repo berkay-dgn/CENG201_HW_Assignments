1b1e375308f4f54d1b885c1a998675279469910d
Berkay62 <berkaydogan224561@gmail.com>
2025-12-22 23:06:49 +0300

    implement of Patient link list

---
+++ src/PatientList.java
@@ -0,0 +1,69 @@
+public class PatientList {
+    private class Node{
+        private Patient data;
+        private Node next;
+        public Node(Patient data){
+            this.data=data;
+        }
+        public Node(Patient data,Node next){
+            this.data=data;
+            this.next=next;
+        }
+    }
+    private Node head;
+    private Node tail;
+    public PatientList(){
+        head =null;
+        tail=null;
+    }
+    public boolean isEmpty(){// bu methodu yazmamın amacı test classda listenin boş olup olmadığını kontrol etmek
+        if(head==tail){
+            return true;
+        }else{
+            return false;
+        }
+
+    }
+    public void addPatient(Patient p){
+        Node node=new Node(p);
+        node.next=head;
+        head=node;
+    }
+    public void printList(){
+        Node temp=head;
+        while(temp!=null){
+            System.out.println(temp.data);
+            temp=temp.next;
+        }
+    }
+    public Patient findPatient(int id){
+        Node temp=head;
+        while(temp!=null){
+            temp=temp.next;
+            if(temp.data.getId()==id){
+                return temp.data;
+            }
+        }
+        return null;
+    }
+    public void removePatient(int id){
+        Node temp=head;
+        Node prev=null;
+        if(isEmpty()){
+            System.out.println("these list is empty ");
+            return;
+        }
+        while(temp!=null){
+            if(temp.data.getId()==id){
+                if(prev==null) {//eğer id head ise
+                    head = head.next;
+                } else{
+                    prev.next=temp.next;
+                }
+            }
+            prev=temp;
+            temp=temp.next;
+        }
+
+    }
+}
