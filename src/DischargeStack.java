b394de889bdf21c9fb156bec3cc429257232ccd1
Berkay62 <berkaydogan224561@gmail.com>
2025-12-22 23:08:14 +0300

    implement of discharge stacks

---
+++ src/DischargeStack.java
@@ -0,0 +1,40 @@
+public class DischargeStack {
+    private class Node{
+        DischargeRecord data;
+        Node link;
+    }
+    Node top;
+    DischargeStack(){this.top=null;}
+    public void push(DischargeRecord record){
+        Node temp=new Node();
+        temp.data=record;
+        temp.link=top;
+        top=temp;
+    }
+    public DischargeRecord peek(){
+        Node temp=top;
+        return temp.data;
+    }
+    public DischargeRecord pop(){
+     if(top==null){
+         System.out.println("this stack is empty");
+         return null;
+     }
+     DischargeRecord x=top.data;
+     top=top.link;
+     return x;
+    }
+    public void Printstack(){
+        Node temp=top;
+        if(top==null){
+            System.out.println("this stack is empty ");
+        }
+        while(temp!=null){
+            System.out.print(temp.data);
+            temp=temp.link;
+            if(temp!=null){
+                System.out.print("-->");
+            }
+        }
+    }
+}
